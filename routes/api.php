09b1b5b7bd56949ebb612b31ac7a84deaa01e298
yousefGo <yousef0sarkar@gmail.com>
2023-03-05 17:25:06 +0300

    flutter api test

---
+++ routes/api.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::get('categoryies',[App\HTTP\Controllers\Api\CategoryController::class,'index']);
+Route::get('categoryies/{category}',[App\HTTP\Controllers\Api\CategoryController::class,'show']);
+
